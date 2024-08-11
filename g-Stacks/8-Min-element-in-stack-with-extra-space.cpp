d2c5befd949b6be61b9f0f571bca0bd74fc03d58
Gurman <gurmankaur0603@gmail.com>
2024-08-11 16:30:48 +0530

    did min stack implementation with extra space

---
+++ g-Stacks/8-Min-element-in-stack-with-extra-space.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+stack<int> s, ss;
+
+void pushInMin(int ele){
+  s.push(ele);
+  if(ss.size()==0 || ss.top()>=ele){
+    // if an element gets pushed twice and deleted once, one should remain, hence >=
+    ss.push(ele);
+    return;
+  }
+}
+
+int popFromMin(){
+  if(s.size() == 0)
+    return -1;
+  int ans = s.top();
+  s.pop();
+  if(ss.top()==ans){
+    ss.pop();
+  }
+  return ans;
+}
+
+int getMin(){
+  if(ss.size()==0)
+    return -1;
+  return ss.top();
+}
+
+int main(){
+  pushInMin(1);
+  pushInMin(2);
+  pushInMin(3);
+  cout<<getMin();
+
+  return 0;
+}
