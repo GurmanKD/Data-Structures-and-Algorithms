75f81f7423236c9fbe5914b5e714948d5325186c
Gurman <gurmankaur0603@gmail.com>
2024-08-12 12:52:03 +0530

    minor changes

--- f-Heaps/5-Top-k-frequent-numbers.cpp
+++ f-Heaps/5-Top-k-frequent-numbers.cpp
@@ -1,8 +1,3 @@
-/*
-Given an unsorted array and two numbers x and k, find k closest values to x.
-Input : arr[] = {10, 2, 14, 4, 7, 6}, x = 5, k = 3 . 
-*/
-
 #include<iostream>
 #include<queue>
 #include<vector>
