0fcdb4bdc81712b4f7154216f6c835771c4d3f28
Gurman <gurmankaur0603@gmail.com>
2024-08-25 11:26:17 +0530

    done

---
+++ i-Trees/B-Medium-Problems/9-Top-view.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<new>
+#include<vector>
+#include<queue>
+#include<map>
+using namespace std;
+
+struct Node{
+  int data;
+  struct Node* left;
+  struct Node* right;
+
+  Node(int val){
+    data = val;
+    left = right = NULL;
+  }
+};
+
+vector<int> topView(Node*root){
+  vector<int>ans;
+  if(root==NULL)return ans;
+  map<int, int>mp; // sorted in inc. order by default
+  queue<pair<Node*, int>>q;
+  q.push({root, 0});
+  while(!q.empty()){
+    auto temp = q.front();
+    q.pop();
+    Node* tempNode = temp.first;
+    int tempCol = temp.second;
+    if(mp.find(tempCol)==mp.end()){
+      mp[tempCol]=tempNode->data;
+    }
+    if(tempNode->left){
+      q.push({tempNode->left, tempCol-1});
+    }
+    if(tempNode->right){
+      q.push({tempNode->right, tempCol+1});
+    }
+  }
+
+  for(auto it:mp){
+    ans.push_back(it.second);
+  }
+
+  return ans;
+}
+
+
+int main(){
+  struct Node*root = new Node(1);
+  root->left = new Node(2);
+  root->right = new Node(3);
+
+  root->left->left = new Node(4);
+
+  vector<int>ans = topView(root);
+
+  for(int i=0; i<ans.size(); i++){
+    cout<<ans[i]<<" ";
+  }
+
+  return 0;
+}
